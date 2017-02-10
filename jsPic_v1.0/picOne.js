acfebbf833b59f5640adbafc6351d916cc0164ca
TianyEmio <534283630@qq.com>
2017-02-10 12:10:13 +0800

    Update picOne.js

--- jsPic_v1.0/picOne.js
+++ jsPic_v1.0/picOne.js
@@ -2,4 +2,7 @@ function showPic(whichsaber){
 	var saber=whichsaber.getAttribute("href");
 	var pic=document.getElementById("holder");
 	pic.setAttribute("src",saber);
+	var title=whichsaber.getAttribute("title");
+	var nameP=document.getElementById("nameP");
+	nameP.firstChild.nodeValue=title;
 }
